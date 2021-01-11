6df866b8994973381a49f0863aff73df12e3a991
Rodrigo Nogueira <rdg6design@gmail.com>
2021-01-11 05:04:33 -0300

    Adicionando redux na aplicação contador

---
+++ main.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': return state + 1
+        case 'DECREMENT': return state - 1
+    }
+    // if(typeof state === 'undefined') {
+    //     return 0
+    // }
+    // if(action.type === 'INCREMENT'){
+    //     return state + 1
+    // }
+    // if (action.type === 'DECREMENT') {
+    //     return state - 1
+    // }
+    return state
+}
+const { createStore } = Redux
+//createStore cria a store, store é um objeto . O objeto principal reservado para todo o estado da aplicação
+console.log(createStore)
+
+const store = createStore(counter)//Criando a store para o reducer counter
+//Metodos da store: dispatch(), getState(), replaceReducer(), subscribe(), Symbol(observable)
+
+console.log(store.getState()) //Metodo getState() retorna o state atual
+
+store.dispatch({ type: 'INCREMENT' }) //Metodo dispatch() dispara uma açao
+store.dispatch({ type: 'INCREMENT' })
+console.log(store.getState())
+
+// console.assert(
+//     counter(0, { type: 'INCREMENT'}) === 1
+// )
+
+// console.assert(
+//     counter(1, { type: 'INCREMENT'}) === 2
+// )
+
+// console.assert(
+//     counter(2, { type: 'DECREMENT'}) === 1
+// )
+// console.assert(
+//     counter(5, { type: 'DECREMENT'}) === 4
+// )
+// console.assert(
+//     counter(3, { type: 'SOMETHING'}) === 3
+// )
+
+// console.assert(
+//     counter(undefined, {}) === 0
+// )
